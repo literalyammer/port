0ba73d61bab0f460ef17c4272cf3c07304c434ff
kane <kjdapple1@gmail.com>
2024-05-21 00:50:20 -0400

    initial fixes

---
+++ src/Logic/PORTRegular.ts
@@ -0,0 +1,5 @@
+export = function (a: number, b: Instance, c: ModuleScript) {
+	print(tostring(a));
+	print(b.Name);
+	print(c.Name);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { findElevator } from "./Dependencies/Allocator";
+import Lantern from "Logic/Lantern";
+import PORTRegular from "Logic/PORTRegular";
 export = function (model: Model, settings: ModuleScript, logic: string[]) {
 	const elevators = model.FindFirstChild("Elevators");
 
@@ -13,11 +14,12 @@ export = function (model: Model, settings: ModuleScript, logic: string[]) {
 			for (const v of Panels.GetChildren()!) {
 				if (v.FindFirstChild("Config") && v.IsA("ModuleScript")) {
 					const config = v;
-					const [res, ran] = pcall(require, "./Logic/PORTRegular.ts");
+					PORTRegular(5, Panels, v);
 				}
 			}
 		}
 	} else if (logic[0] === "LANTERN") {
 		const Lanterns = model.FindFirstChild("Lanterns")!;
+		Lantern(true);
 	}
 };
