71df4a17457e298f25cfe99470b76394f22bbba9
Yam-Dev <kjdapple1@gmail.com>
2024-05-21 00:08:48 -0400

    modified:   .vscode/settings.json 	modified:   default.project.json 	new file:   src/PORTMainModule/Logic/Lantern.ts 	modified:   src/PORTMainModule/index.ts

--- src/PORTMainModule/index.ts
+++ src/PORTMainModule/index.ts
@@ -1,23 +1,23 @@
 import { findElevator } from "./Dependencies/Allocator";
 export = function (model: Model, settings: ModuleScript, logic: string[]) {
-	const branch = Array
-	const elevators = model.FindFirstChild("Elevators")
-	
-	if (!elevators) {print("No Elevators Found! Please add them to the model."); return;}
+	const elevators = model.FindFirstChild("Elevators");
+
+	if (!elevators) {
+		print("No Elevators Found! Please add them to the model.");
+		return;
+	}
 	if (logic[0] === "FULL") {
-		const Panels = model.FindFirstChild("Panels")!
-		const Lanterns = model.FindFirstChild("Lanterns")
+		const Panels = model.FindFirstChild("Panels")!;
+		const Lanterns = model.FindFirstChild("Lanterns");
 		if (Panels !== undefined) {
-			for (const [i,v] of pairs(Panels.GetChildren()!)) {
+			for (const v of Panels.GetChildren()!) {
 				if (v.FindFirstChild("Config") && v.IsA("ModuleScript")) {
-					const config = require(v)
+					const config = v;
+					const [res, ran] = pcall(require, "./Logic/PORTRegular.ts");
 				}
 			}
-		}	
+		}
+	} else if (logic[0] === "LANTERN") {
+		const Lanterns = model.FindFirstChild("Lanterns")!;
 	}
-	else if (logic[0] === "LANTERN"){
-
-	}
-}
-
-
+};
